36cc0ac2183db74b5bc1dd396f81891a109d6252
Takahiro Shinchi <takahiro.shinchi@supership.jp>
2023-01-16 11:06:15 +0900

    tailwind.css 修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import "./globals.css";
+import "./styles/globals.css";
 
 import { Tab } from "app/components/Tab";
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
   important: "#__next",
   theme: { extend: {} },
   plugins: [require("@tailwindcss/typography")],

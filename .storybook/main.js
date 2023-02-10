f481ac0132a5ccb97b325e6a35be5c98dcc8eacb
Takahiro Shinchi <takahiro.shinchi@supership.jp>
2023-02-10 13:40:01 +0900

    fix-10 chomatic CI

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,10 +3,6 @@ module.exports = {
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-postcss",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   framework: "@storybook/react",
 };

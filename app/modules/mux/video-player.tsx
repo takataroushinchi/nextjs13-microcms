d8e745874cb629fd36bee70ab6e9ed1e45618ff3
Takahiro Shinchi <takahiro.shinchi@supership.jp>
2023-02-12 11:30:06 +0900

    test react-player

--- app/modules/mux/video-player.tsx
+++ app/modules/mux/video-player.tsx
@@ -1,12 +1,22 @@
 "use client";
 
+import ReactPlayer from "react-player";
+
 export const VideoPlayer = () => {
+  const putVideoPlayLog = () => {
+    console.log("VideoPlayer Log");
+  };
   return (
-    <video controls width="100%" height="auto" controlsList="nodownload">
-      <source
-        src="https://cdn.search.auone.jp/static/web/permanent/img/amp/story01/cover.mp4"
-        type="video/mp4"
-      />
-    </video>
+    <ReactPlayer
+      url={
+        "https://cdn.search.auone.jp/static/web/permanent/img/amp/story01/cover.mp4"
+      }
+      className="react-player"
+      controls={true}
+      width="100%"
+      height="100%"
+      playing={false}
+      onStart={() => putVideoPlayLog()}
+    />
   );
 };

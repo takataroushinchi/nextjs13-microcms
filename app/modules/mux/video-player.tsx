b52494d44488d17d4b5408098982c790a642d618
Takahiro Shinchi <takahiro.shinchi@supership.jp>
2023-02-12 09:46:48 +0900

    del MUX

--- app/modules/mux/video-player.tsx
+++ app/modules/mux/video-player.tsx
@@ -1,15 +1,12 @@
 "use client";
-import MuxPlayer from "@mux/mux-player-react";
+
 export const VideoPlayer = () => {
   return (
-    <MuxPlayer
-      streamType="on-demand"
-      playbackId="xcxFcWGU6JA00ELM7Snwv3ujgtYjijUzxux35Sk3ltsI"
-      metadata={{
-        video_id: "video-id-54321",
-        video_title: "Test video title",
-        viewer_user_id: "user-id-007",
-      }}
-    />
+    <video controls width="100%" height="auto" controlsList="nodownload">
+      <source
+        src="https://cdn.search.auone.jp/static/web/permanent/img/amp/story01/cover.mp4"
+        type="video/mp4"
+      />
+    </video>
   );
 };

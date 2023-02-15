1199f7458f6dbcaf5318a5cc6334cb32b35f7477
Takahiro Shinchi <takahiro.shinchi@supership.jp>
2023-02-15 18:03:59 +0900

    Ctrl/⌘ + J

--- app/modules/themes-switch.tsx
+++ app/modules/themes-switch.tsx
@@ -17,19 +17,22 @@ export default function ThemesSwitchSt() {
   }, [colorScheme]);
 
   return (
-    <button
-      aria-label="DarkModeToggle"
-      type="button"
-      className="grid h-12 w-12 place-items-center rounded-full bg-slate-50 p-4 ring-2 ring-slate-900/5 hover:bg-slate-600 hover:ring-4 dark:bg-slate-600/30 dark:hover:bg-sky-600 md:p-1"
-      onClick={() => toggleColorScheme()}
-    >
-      <>
-        {dark ? (
-          <Sun height={"25"} width={"25"} />
-        ) : (
-          <Moon height={"25"} width={"25"} />
-        )}
-      </>
-    </button>
+    <div className="flex items-center gap-2">
+      <button
+        aria-label="DarkModeToggle"
+        type="button"
+        className="grid h-12 w-12 place-items-center rounded-full bg-slate-50 p-4 ring-2 ring-slate-900/5 hover:bg-slate-600 hover:ring-4 dark:bg-slate-600/30 dark:hover:bg-sky-600 md:p-1"
+        onClick={() => toggleColorScheme()}
+      >
+        <>
+          {dark ? (
+            <Sun height={"25"} width={"25"} />
+          ) : (
+            <Moon height={"25"} width={"25"} />
+          )}
+        </>
+      </button>
+      <span className="whitespace-nowrap text-xs">Ctrl / ⌘ + J</span>
+    </div>
   );
 }

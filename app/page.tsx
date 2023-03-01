cd7bcc1c5c8761972da7978bb4d4a1ee72aebae4
Takahiro Shinchi <takahiro.shinchi@supership.jp>
2023-03-01 18:40:30 +0900

    Next v13.2.3

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import { getBlogList } from "lib/microcms";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
 import { GenreList } from "@/app/modules/genre-list";
 
 import styles from "./page.module.css";
+const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "デザイン管理ドキュメント",
   description: "デザイン管理ドキュメント（S4,au,KDDI）",
   icons: {
@@ -22,7 +25,7 @@ export default async function Home() {
   return (
     <main className={`${styles.main} bg-gray-200 pl-2`}>
       <h1
-        className={`mt-10 scroll-m-20 py-2 text-2xl font-semibold tracking-tight transition-colors first:mt-0`}
+        className={`${inter.className} mt-10 scroll-m-20 py-2 text-2xl font-semibold tracking-tight transition-colors first:mt-0`}
       >
         デザイン管理ドキュメント
       </h1>

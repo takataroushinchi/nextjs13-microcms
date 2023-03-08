c14547793efc556e7d1500a14c69c2c2db3503ca
Takahiro Shinchi <takahiro.shinchi@supership.jp>
2023-03-08 15:00:11 +0900

    検索エンジンにインデックスされないように HTTP ヘッダーを設定

--- next.config.js
+++ next.config.js
@@ -8,6 +8,21 @@ const nextConfig = {
   images: {
     domains: ["images.microcms-assets.io", "picsum.photos"],
   },
+  async headers() {
+    const headers = [];
+    // if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+    headers.push({
+      headers: [
+        {
+          key: "X-Robots-Tag",
+          value: "noindex",
+        },
+      ],
+      source: "/:path*",
+    });
+    // }
+    return headers;
+  },
 };
 
 module.exports = nextConfig;
